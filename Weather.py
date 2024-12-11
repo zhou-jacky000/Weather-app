8bae0d2ba19cae4b0f00b70ca30d037aaf17ae60
CHOUJINYU\jacky <jacky556644645@gmail.com>
2024-12-11 14:01:23 +0800

    Add more weather recent_search

--- Weather.py
+++ Weather.py
@@ -1,6 +1,6 @@
 import sys
 import requests
-from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout)
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout,QListWidget)
 from PyQt5.QtCore import Qt
 
 class WeatherApp(QWidget):
@@ -15,7 +15,12 @@ class WeatherApp(QWidget):
         self.humidity_label = QLabel(self)
         self.pressure_label = QLabel(self)
         self.wind_label = QLabel(self)
+        self.recent_searches_label = QLabel("Recent Searches:", self)
+        self.recent_searches_list = QListWidget(self)
         self.initUI()
+
+        # Store recent searches
+        self.recent_searches = []
     
     def initUI(self):
         self.setWindowTitle("Weather APP")
@@ -31,11 +36,23 @@ class WeatherApp(QWidget):
         vbox.addWidget(self.humidity_label)
         vbox.addWidget(self.pressure_label)
         vbox.addWidget(self.wind_label)
+        vbox.addWidget(self.recent_searches_label)
+        vbox.addWidget(self.recent_searches_list)
 
         self.setLayout(vbox)
 
         # Center alignments
-        for widget in [self.city_label, self.city_input, self.temperature_label, self.emoji_label, self.description_label, self.humidity_label, self.pressure_label, self.wind_label]:
+        for widget in [
+            self.city_label,
+            self.city_input,
+            self.temperature_label,
+            self.emoji_label,
+            self.description_label,
+            self.wind_label,
+            self.humidity_label,
+            self.pressure_label,
+            self.recent_searches_label,
+        ]:
             widget.setAlignment(Qt.AlignCenter)
 
         # Object names for styling
@@ -48,6 +65,8 @@ class WeatherApp(QWidget):
         self.humidity_label.setObjectName("humidity_label")
         self.pressure_label.setObjectName("pressure_label")
         self.wind_label.setObjectName("wind_label")
+        self.recent_searches_label.setObjectName("recent_searches_label")
+        self.recent_searches_list.setObjectName("recent_searches_list")
 
         # Apply styles
         self.setStyleSheet("""
@@ -72,17 +91,34 @@ class WeatherApp(QWidget):
                 font-size: 100px;
                 font-family: Segoe UI Emoji;
             }
-            QLabel#description_label, QLabel#humidity_label, QLabel#pressure_label, QLabel#wind_label {
+            QLabel#description_label {
                 font-size: 30px;
+            }
+            QLabel#wind_label, QLabel#humidity_label, QLabel#pressure_label {
+                font-size: 20px;
+                color: #555;
+            }
+            QLabel#recent_searches_label {
+                font-size: 25px;
+                font-weight: bold;
+                margin-top: 20px;
+            }
+            QListWidget#recent_searches_list {
+                font-size: 20px;
             }
         """)
 
         # Connect button click
         self.get_weather_button.clicked.connect(self.get_weather)
+        self.recent_searches_list.itemClicked.connect(self.load_recent_search)
 
     def get_weather(self):
         api_key = "6231c5e4b2f9be27e364fbd5ef9a4b5e"
         city = self.city_input.text()
+        if not city:
+            self.display_error("Please enter a city name.")
+            return
+
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
         try:
@@ -91,11 +127,21 @@ class WeatherApp(QWidget):
             data = response.json()
 
             if data["cod"] == 200:
+                self.add_to_recent_searches(city)
                 self.display_weather(data)
             else:
                 self.display_error("City not found.")
         except requests.exceptions.RequestException as e:
             self.display_error(f"Error: {e}")
+
+    def add_to_recent_searches(self, city):
+        if city not in self.recent_searches:
+            self.recent_searches.append(city)
+            self.recent_searches_list.addItem(city)
+
+    def load_recent_search(self, item):
+        self.city_input.setText(item.text())
+        self.get_weather()
 
     def display_weather(self, data):
         # Convert temperature
